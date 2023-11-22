2be6fb7026f9ce480330c7ab00d3a3395658c2a3
Emrul Hoque <emrul.hoque@gmail.com>
2023-11-22 18:29:58 +0600

    practice

---
+++ colour_list.py
@@ -0,0 +1,11 @@
+import os
+
+'''Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]'''
+
+os.system("cls")
+
+color_list = ["Red","Green","White" ,"Black"]
+
+print("\n", color_list[0], " and ", color_list[-1], "\n")
+
---
+++ sphere_vol.py
@@ -0,0 +1,13 @@
+import os
+import math
+
+'''Write a Python program to get the volume of a sphere with radius six. 
+The volume of the sphere is : V = 4/3 × π × r3 = π × d3/6'''
+
+r = int(input("Enter the radius of the sphere:\t"))
+v = 4/3 * math.pi * r**3
+
+print(f"\nThe volume of the sphere is : \t{v}\n")
+
+
+
